18bf6ab853dae8ef50d6ac172a356726d0a9b2e9
Semir <sblekic1@etf.unsa.ba>
2022-12-01 17:33:05 +0100

    DIP after

---
+++ Dependency-Inversion-Principle/after.py
@@ -0,0 +1,143 @@
+from abc import ABC, abstractmethod
+
+
+class Order:
+
+    def __init__(self):
+        self.items = []
+        self.quantities = []
+        self.prices = []
+        self.status = "open"
+
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i] * self.prices[i]
+        return total
+
+
+# we could solve it by creating another abstract authorizer class
+# that we can pass to the payment processors
+
+
+class Authorizer(ABC):
+    @abstractmethod
+    def is_authorized(self) -> bool:
+        pass
+
+
+class Authorizer_SMS(Authorizer):
+
+    def __init__(self):
+        self.authorized = False
+
+    def verify_code(self, code):
+        print(f"Verifying SMS code {code}")
+        self.authorized = True
+
+    def is_authorized(self) -> bool:
+        return self.authorized
+
+
+class Authorizer_Google(Authorizer):
+
+    def __init__(self):
+        self.authorized = False
+
+    def verify_code(self, code):
+        print(f"Verifying Google auth code {code}")
+        self.authorized = True
+
+    def is_authorized(self) -> bool:
+        return self.authorized
+
+# now we can add other things that make this easier to use
+# for example, new method like below:
+
+
+class Authorizer_Robot(Authorizer):
+
+    def __init__(self):
+        self.authorized = False
+
+    def not_a_robot(self):
+        self.authorized = True
+
+    def is_authorized(self) -> bool:
+        return self.authorized
+
+
+class PaymentProcessor(ABC):
+
+    @abstractmethod
+    def pay(self, order):
+        pass
+
+
+class DebitPaymentProcessor(PaymentProcessor):
+
+    def __init__(self, security_code, authorizer: Authorizer):  # now it depends on abstract class
+       # it's a type of Authorizer!
+        self.security_code = security_code
+        self.authorizer = authorizer
+
+    def pay(self, order):
+        if not self.authorizer.is_authorized():
+            raise Exception("Not authorized")
+        print("Processing debit payment type")
+        print(f"Verifying security code: {self.security_code}")
+        order.status = "paid"
+
+
+class CreditPaymentProcessor(PaymentProcessor):
+
+    def __init__(self, security_code):
+        self.security_code = security_code
+
+    def pay(self, order):
+        print("Processing credit payment type")
+        print(f"Verifying security code: {self.security_code}")
+        order.status = "paid"
+
+
+class PaypalPaymentProcessor(PaymentProcessor):
+
+    def __init__(self, email_address, authorizer: Authorizer):
+        self.email_address = email_address
+        self.authorizer = authorizer
+
+    def pay(self, order):
+        if not self.authorizer.is_authorized():
+            raise Exception("Not authorized")
+        print("Processing paypal payment type")
+        print(f"Using email address: {self.email_address}")
+        order.status = "paid"
+
+
+order = Order()
+order.add_item("Keyboard", 1, 50)
+order.add_item("SSD", 1, 150)
+order.add_item("USB cable", 2, 5)
+
+print(order.total_price())
+authorizer = Authorizer_Robot()
+# authorizer.verify_code(465839)
+authorizer.not_a_robot()
+processor = PaypalPaymentProcessor("hi@email.com", authorizer)
+processor.pay(order)
+
+print("\nAnother one: \n")
+order2 = Order()
+order2.add_item("Galaxy Watch", 1, 500)
+order2.add_item("Gaming PC", 1, 1150)
+order2.add_item("Skateboard", 2, 170)
+print(order2.total_price())
+authorizer2 = Authorizer_Google()
+authorizer2.verify_code("000000")
+processor2 = DebitPaymentProcessor("000000", authorizer2)
+processor2.pay(order2)
