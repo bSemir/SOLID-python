af4705754f30cfe554da313fe2e42f68d188d6ce
Semir <sblekic1@etf.unsa.ba>
2022-12-01 15:01:02 +0100

    OCP after

---
+++ Open-Closed-Principle/after.py
@@ -0,0 +1,67 @@
+from abc import ABC, abstractmethod
+
+
+class Order:
+
+    def __init__(self):
+        self.items = []
+        self.quantities = []
+        self.prices = []
+        self.status = "open"
+
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i] * self.prices[i]
+        return total
+
+
+# optimally, what we could do is to create structure of classes and subclasses
+# so that we can just define a new subclass for each new payment type!
+
+
+# this way we don't violate the OCP
+class PaymentProcessor(ABC):
+
+    # ovo je kao virtual u c++?
+    @abstractmethod
+    def pay(self, order, security_code):
+        pass
+
+
+class DebitPaymentProcessor(PaymentProcessor):
+    def pay(self, order, security_code):
+        print("Processing debit payment type")
+        print(f"Verifying security code: {security_code}")
+        order.status = "paid"
+
+
+class CreditPaymentProcessor(PaymentProcessor):
+    def pay(self, order, security_code):
+        print("Processing credit payment type")
+        print(f"Verifying security code: {security_code}")
+        order.status = "paid"
+
+# another example:
+
+
+class PaypalPaymentProcessor(PaymentProcessor):
+    def pay(self, order, security_code):
+        print("Processing Paypal payment type")
+        print(f"Verifying security code: {security_code}")
+        order.status = "paid"
+
+
+order = Order()
+order.add_item("Keyboard", 1, 50)
+order.add_item("SSD", 1, 150)
+order.add_item("USB cable", 2, 5)
+
+print(order.total_price())
+processor = DebitPaymentProcessor()  # PaypalPaymentProcessor()
+processor.pay(order, "0372846")
